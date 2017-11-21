bcc1d2eba0379d5ee7048dbd89e03117ed5d3a29
Peter <montmarx@gmail.com>
2017-11-21 14:28:04 +0200

    Back menu Arrow

---
+++ Assets/Scripts/ReturnButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;  
+
+public class ReturnButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	public void ReturnMainMenu()
+	{
+		SceneManager.LoadScene ("MainMenu");
+	}
+}
